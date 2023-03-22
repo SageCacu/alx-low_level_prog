847b6dd658339d729b207e780e42317d30c2d04f
SageCacu <billysage12@gmail.com>
2023-03-22 09:46:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main : main function
- * return : (0):always (success)
-*/
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle,");
 	return (0);
 }
