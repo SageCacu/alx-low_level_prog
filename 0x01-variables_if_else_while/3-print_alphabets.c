bd7dd54327cf71439ea94383e2645288dfd03aa7
SageCacu <billysage12@gmail.com>
2023-03-29 16:40:29 -0700

    lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,9 @@ int main(void)
 	char j;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-		putchar('i');
+		putchar(i);
 	for (j = 'A' ; j <= 'Z' ; j++)
-		putchar('j');
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
