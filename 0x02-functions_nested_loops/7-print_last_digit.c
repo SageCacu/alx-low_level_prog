6d00a111a030e42e954ebda42ad3784a86691fdc
SageCacu <billysage12@gmail.com>
2023-04-01 17:13:06 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - last digit
+ *
+ * @i: parameter of function
+ *
+ * Return: k
+ */
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
+}
