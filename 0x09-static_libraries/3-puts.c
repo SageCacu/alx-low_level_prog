22d6b874c92b030755b5073e25b6519bf722b570
SageCacu <billysage12@gmail.com>
2023-04-08 19:20:46 -0700

    task 1 update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * _puts - _putschar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+all statistics tasks
